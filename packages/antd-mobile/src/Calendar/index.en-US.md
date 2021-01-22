6ac1eec7585f32cf5f23850fc00e72c1a54d56e2
csj1328059093 <1328059093@qq.com>
2021-01-22 15:00:34 +0800

    feat: calendar

---
+++ packages/antd-mobile/src/Calendar/index.en-US.md
@@ -0,0 +1,12 @@
+---
+title: Calendar
+nav:
+  title: antd-mobile
+  path: /antd
+---
+
+Used to select a date range.
+
+<code src="./demo/basic.tsx" />
+
+<API/>
---
+++ packages/antd-mobile/src/Calendar/index.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import ACalendar from 'antd-mobile/lib/calendar';
+import { withError, useTracker } from '@alitajs/tracker';
+import { CalendarPropsType } from './PropsType';
+import './index.less';
+
+export const Calendar: React.FC<CalendarPropsType> = (props) => {
+  const { onCancel, onConfirm, onSelect, onSelectHasDisableDate, ext, ...other } = props;
+
+  const log = useTracker(Calendar.displayName, {
+    ext,
+  });
+
+  return (
+    <ACalendar
+      {...other}
+      onCancel={(e) => {
+        onCancel && onCancel(e);
+        log('onCancel');
+      }}
+      onConfirm={(e) => {
+        onConfirm && onConfirm(e);
+        log('onConfirm');
+      }}
+      onSelect={(e) => {
+        onSelect && onSelect(e);
+        log('onSelect');
+      }}
+      onSelectHasDisableDate={(e) => {
+        onSelectHasDisableDate && onSelectHasDisableDate(e);
+        log('onSelectHasDisableDate');
+      }}
+    >
+    </ACalendar>
+  );
+};
+
+Calendar.displayName = 'Calendar';
+
+export default withError(Calendar);
