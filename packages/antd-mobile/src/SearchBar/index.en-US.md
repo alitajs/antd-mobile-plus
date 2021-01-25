aeadab6e14fe029c4c712d55241d9b4a3934f2ab
csj1328059093 <1328059093@qq.com>
2021-01-25 15:26:24 +0800

    feat: search-bar

---
+++ packages/antd-mobile/src/SearchBar/index.en-US.md
@@ -0,0 +1,12 @@
+---
+title: Components
+nav:
+  title: antd-mobile
+  path: /antd
+---
+
+Normally located below NavBar, the activation status is exited by the Cancel button.
+
+<code src="./demo/basic.tsx" />
+
+<API/>
---
+++ packages/antd-mobile/src/SearchBar/index.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import ASearchBar from 'antd-mobile/lib/search-bar';
+import { withError, useTracker } from '@alitajs/tracker';
+import { SearchBarPropsType } from './PropsType';
+import './index.less';
+
+export const SearchBar: React.FC<SearchBarPropsType> = (props) => {
+  const { onChange,onSubmit,onBlur,onCancel,onClear,onFocus, ext, ...other } = props;
+
+  const log = useTracker(SearchBar.displayName, {
+    ext,
+  });
+
+  return (
+    <ASearchBar
+      {...other}
+      onChange={(e) => {
+        onChange && onChange(e);
+        log('onChange');
+      }}
+      onSubmit={(e) => {
+        onSubmit && onSubmit(e);
+        log('onSubmit');
+      }}
+      onBlur={(e) => {
+        onBlur && onBlur(e);
+        log('onBlur');
+      }}
+      onCancel={(e) => {
+        onCancel && onCancel(e);
+        log('onCancel');
+      }}
+      onClear={(e) => {
+        onClear && onClear(e);
+        log('onClear');
+      }}
+      onFocus={(e) => {
+        onFocus && onFocus(e);
+        log('onFocus');
+      }}
+    >
+    </ASearchBar>
+  );
+};
+
+SearchBar.displayName = 'SearchBar';
+
+export default withError(SearchBar);
