a9922c07da1b9d8a1241d1bff669d89ed9138a06
csj1328059093 <1328059093@qq.com>
2021-01-25 13:46:26 +0800

    feat: pagination

---
+++ packages/antd-mobile/src/Pagination/PropsType.ts
@@ -0,0 +1,49 @@
+import { PaginationProps } from 'antd-mobile/es/pagination';
+
+export interface PaginationPropsType extends PaginationProps {
+  /**
+   * @description       形态，可选`button`,`number`,`pointer`
+   * @description.en-US the mode of `Pagination` which can be one of `button`,`number`,`pointer`
+   * @default           `button`
+   */
+  mode?: PaginationProps['mode'];
+  /**
+   * @description       当前页号
+   * @description.en-US current page index
+   * @default           1
+   */
+  current?: PaginationProps['current'];
+  /**
+   * @description       数据总数
+   * @description.en-US total number of data
+   * @default           0
+   */
+  total?: PaginationProps['total'];
+  /**
+   * @description       是否隐藏数值
+   * @description.en-US whether to hide number
+   * @default           false
+   */
+  simple?: PaginationProps['simple'];
+  /**
+   * @description       禁用状态
+   * @description.en-US whether is disabled
+   * @default           false
+   */
+  disabled?: PaginationProps['disabled'];
+  /**
+   * @description       国际化, 可以覆盖全局`LocaleProvider`的配置
+   * @description.en-US [i18n](/components/locale-provider/) setting, you can override the configuration of the global `LocaleProvider
+   */
+  locale?: PaginationProps['locale'];
+  /**
+   * @description       change 事件触发的回调函数
+   * @description.en-US invoked with the new index when the value changes.
+   */
+  onChange?: PaginationProps['onChange'];
+  /**
+   * @description       埋点自定义数据
+   * @description.en-US tracker
+   */
+  ext?: any;
+}
