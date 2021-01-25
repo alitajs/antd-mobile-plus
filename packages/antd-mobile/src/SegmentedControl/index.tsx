a44cd9806590c4339918c5f5a10780281da741e5
csj1328059093 <1328059093@qq.com>
2021-01-25 16:24:42 +0800

    feat: segmented-control

---
+++ packages/antd-mobile/src/SegmentedControl/index.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import ASegmentedControl from 'antd-mobile/lib/segmented-control';
+import { withError, useTracker } from '@alitajs/tracker';
+import { SegmentedControlPropsType } from './PropsType';
+import './index.less';
+
+export const SegmentedControl: React.FC<SegmentedControlPropsType> = (props) => {
+  const { onChange, onValueChange, ext, ...other } = props;
+
+  const log = useTracker(SegmentedControl.displayName, {
+    ext,
+  });
+
+  return (
+    <ASegmentedControl
+      {...other}
+      onChange={(e) => {
+        onChange && onChange(e);
+        log('onChange');
+      }}
+      onValueChange={(e) => {
+        onValueChange && onValueChange(e);
+        log('onValueChange');
+      }}
+    >
+    </ASegmentedControl>
+  );
+};
+
+SegmentedControl.displayName = 'SegmentedControl';
+
+export default withError(SegmentedControl);
---
+++ packages/antd-mobile/src/SegmentedControl/index.zh-CN.md
@@ -0,0 +1,12 @@
+---
+title: 分段器
+nav:
+  title: antd-mobile
+  path: /antd
+---
+
+由至少 2 个分段控件组成，用作不同视图的显示；是 iOS 的推荐组件。
+
+<code src="./demo/basic.tsx" />
+
+<API/>
