6a1e398e34ae51df7085f6ee06d20eadc697c43b
qi.futao <qi.futao@iwhalecloud.com>
2021-03-24 09:53:31 +0800

    feat:新增文本省略等8个组件

---
+++ packages/antd-mobile-plus/src/NewsStatistic/PropsType.ts
@@ -0,0 +1,51 @@
+/*
+ * @Description: 
+ * @Autor: qifutao
+ * @Date: 2021-03-19 15:21:12
+ */
+export interface NewsStatisticType {
+  /**
+   * @description 文本
+   */
+  text: string;
+
+  /**
+   * @description 阅读量
+   */
+  readNum?: number;
+
+  /**
+   * @description 点赞数
+   */
+  starNum?: number;
+
+  /**
+   * @description 行内样式
+   */
+  style?: React.CSSProperties;
+
+  /**
+   * @description 字号
+   */
+  fontSize?: number;
+
+  /**
+   * @description 类名
+   */
+  className?: string;
+
+  /**
+   * @description 点赞状态
+   */
+  active?: boolean;
+
+  /**
+   * @description 点赞回调
+   */
+  onClick: (active: boolean) => void;
+
+/**
+ * @description 是否显示 Star 数量
+ */
+  showStarNum?: boolean;
+}
---
+++ packages/antd-mobile-plus/src/Tab/index.md
@@ -0,0 +1,24 @@
+---
+title: 功能页签
+group: 
+  title: 功能页签
+nav:
+  title: 组件
+  path: /components
+---
+
+# QRCode
+### 展示
+
+<code src="./demos/demo.tsx" />
+
+### 参数
+
+<API />
+
+###
+
+| 属性    | 说明                 | 类型   | 默认值 |
+| ------- | -------------------- | ------ | ------ |
+| tabs | 数据 | Array | - |
+| onChange | 当tab发生变化时回调 | Function | - |
