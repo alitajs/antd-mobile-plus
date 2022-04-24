f7fe18b927da772e144744171beb19487620b6e0
LimMem <qin.chuanlong@iwhalecloud.com>
2022-04-24 09:46:56 +0800

    feat: 优化Table兼容宽度字符串类型

--- packages/antd-mobile-plus/src/Table/index.tsx
+++ packages/antd-mobile-plus/src/Table/index.tsx
@@ -7,7 +7,13 @@ import './index.less';
 const prefixCls = 'alita-table';
 
 const Table: FC<TableType> = (props) => {
-  const { dataSource = [], columns = [], twoDimension = false, titleBackground = '#f7f7f7', titleColor = '#333' } = props;
+  const {
+    dataSource = [],
+    columns = [],
+    twoDimension = false,
+    titleBackground = '#f7f7f7',
+    titleColor = '#333',
+  } = props;
   const container = useRef<any>(null);
   const [trWidth, setTrWidth] = useState<string>('');
   const [pxRatio, setPxRatio] = useState(1);
@@ -18,52 +24,91 @@ const Table: FC<TableType> = (props) => {
       clientWidth = container.current.clientWidth;
     }
     let width = 0;
-    if(Array.isArray(columns)) {
+    if (Array.isArray(columns)) {
       columns.map((item) => {
-        width += item.width || 0;
+        width += parseFloat(`${item.width}`);
       });
     }
     if (width < clientWidth) {
       setPxRatio(clientWidth / width);
       width = clientWidth;
-    } 
+    }
     setTrWidth(`${width}px`);
-  },[columns]);
+  }, [columns]);
 
   return (
     <div className={prefixCls} ref={container}>
-      <div className={`${prefixCls}-tr`} style={{ width: `${trWidth}`}}>
-        {
-          columns && columns.map((v, index) => {
+      <div className={`${prefixCls}-tr`} style={{ width: `${trWidth}` }}>
+        {columns &&
+          columns.map((v, index) => {
             if (twoDimension && index === 0 && Array.isArray(v.title)) {
               return (
-                <div className={classnames(`${prefixCls}-th`, `${prefixCls}-th-complex`)} key={index} style={{ width: `${(v.width || 100) * pxRatio}px`, background: `${titleBackground}`, color: `${titleColor}` }}>
+                <div
+                  className={classnames(
+                    `${prefixCls}-th`,
+                    `${prefixCls}-th-complex`,
+                  )}
+                  key={index}
+                  style={{
+                    width: `${(v.width || 100) * pxRatio}px`,
+                    background: `${titleBackground}`,
+                    color: `${titleColor}`,
+                  }}
+                >
                   <span className="top">{v.title[0]}</span>
                   <span className="bottom">{v.title[1]}</span>
                 </div>
-              )
+              );
             }
-            return <div className={`${prefixCls}-th`} key={index} style={{ width: `${(v.width || 100) * pxRatio}px`, background: `${titleBackground}`, color: `${titleColor}`}}>{v.title}</div>
-          })
-        }
+            return (
+              <div
+                className={`${prefixCls}-th`}
+                key={index}
+                style={{
+                  width: `${(v.width || 100) * pxRatio}px`,
+                  background: `${titleBackground}`,
+                  color: `${titleColor}`,
+                }}
+              >
+                {v.title}
+              </div>
+            );
+          })}
       </div>
-      {
-        dataSource && dataSource.map((v, index) => (
-          <div className={`${prefixCls}-tr`} key={index} style={{ width: `${trWidth}`}}>
-            {
-              columns && columns.map((item, tIndex) => (
+      {dataSource &&
+        dataSource.map((v, index) => (
+          <div
+            className={`${prefixCls}-tr`}
+            key={index}
+            style={{ width: `${trWidth}` }}
+          >
+            {columns &&
+              columns.map((item, tIndex) => (
                 <Fragment key={tIndex}>
                   <div
-                    className={twoDimension && tIndex === 0 ? `${prefixCls}-th` : `${prefixCls}-td`}
-                    style={item.align ?
-                    { width: `${(item.width || 100) * pxRatio}px`, alignItems: `${item.align}`, justifyContent: `${item.align}` } :
-                    { width: `${(item.width || 100) * pxRatio}px` }}>{item.render ? item.render(v[item.dataIndex], v, tIndex) : v[item.dataIndex]}</div>
+                    className={
+                      twoDimension && tIndex === 0
+                        ? `${prefixCls}-th`
+                        : `${prefixCls}-td`
+                    }
+                    style={
+                      item.align
+                        ? {
+                            width: `${(item.width || 100) * pxRatio}px`,
+                            alignItems: `${item.align}`,
+                            justifyContent: `${item.align}`,
+                          }
+                        : { width: `${(item.width || 100) * pxRatio}px` }
+                    }
+                  >
+                    {item.render
+                      ? item.render(v[item.dataIndex], v, tIndex)
+                      : v[item.dataIndex]}
+                  </div>
                 </Fragment>
-              ))
-            }
+              ))}
           </div>
-        ))
-      }
+        ))}
     </div>
   );
 };
